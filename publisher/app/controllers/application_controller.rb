1fc458b3c06941735cd6b1dcb1d1915fdbd5beb8
Leon Du <ld@ninjaconcept.com>
2010-11-22 22:20:56 +0800

    added authlogic for publisher app

--- publisher/app/controllers/application_controller.rb
+++ publisher/app/controllers/application_controller.rb
@@ -3,8 +3,49 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  protect_from_forgery
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  filter_parameter_logging :password
+
+  helper_method :current_user
+
+  protected
+
+  # before filter to make sure user is logged in
+  def login_required
+    unless current_user
+      store_location
+      flash[:error] = "You must be logged in to access this page."
+      redirect_to login_path
+      return false
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  def store_location
+    session[:return_to] = request.request_uri
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
+
+  private
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
 end
